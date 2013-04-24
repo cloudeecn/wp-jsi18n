021d5726265849b72423b39b2696561a3f4ea9de
cloudeecn <cloudeecn@1de1ff53-06d2-dd94-f62b-d60294cf4371>
2013-04-24 04:49:54 +0000

    修改children算法

--- trunk/jsi18n.js
+++ trunk/jsi18n.js
@@ -1,6 +1,6 @@
 jQuery(document).ready(function($) {
 	$(".jsi18n-choose").each(function() {
-		$(this).find("> *").hide();
+		$(this).children().hide();
 		var found=false;
 		for(var i=0,max=jsi18n.length;i<max;i++){
 			$(this).find("> .jsi18n-"+jsi18n[i].lang).each(function(){
